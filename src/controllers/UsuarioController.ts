223fe9f707f6d94f51364a5480d3742a985e061f
Thales <devthalespaloski@gmail.com>
2026-03-06 11:22:37 -0300

    refact: try catch e middlware

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -1,12 +1,16 @@
 import UsuarioService from "../services/UsuarioService.ts";
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 const usuarioService = new UsuarioService();
 
 class UsuarioController {
-    async cadastrarUsuario (req: Request, res: Response) {
-        const novoUsuario = await usuarioService.cadastrarUsuario(req.body);
-        return res.status(201).json(novoUsuario);
+    async cadastrarUsuario (req: Request, res: Response, next: NextFunction) {
+        try {
+            const novoUsuario = await usuarioService.cadastrarUsuario(req.body);
+            return res.status(201).json(novoUsuario);
+        } catch (error) {
+            next(error)
+        }
     }
 }
 
